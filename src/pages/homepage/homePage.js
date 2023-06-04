6a98cfa0f85775c4d2087dc7c6f606e66a4ba6a0
AliMasykur10 <alimasykur2001@gmail.com>
2023-06-04 20:19:15 +0700

    first commmit

---
+++ src/pages/homepage/homePage.js
@@ -0,0 +1,24 @@
+import "./homepage.css";
+
+export default function HomePage() {
+  return (
+    <>
+      <nav>
+        <div className="navbar">
+          <div className="nav-first-comp">
+            <ul>
+              <li>haji</li>
+              <li>nasional</li>
+              <li>surabaya</li>
+              <li>sepak bola</li>
+              <li>entertainment</li>
+            </ul>
+          </div>
+          <div className="nav-second-comp">
+            <button>lainnya</button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
