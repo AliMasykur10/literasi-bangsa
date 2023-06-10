f8bfc916615e4453a7d05d800597153eaac12490
AliMasykur10 <alimasykur2001@gmail.com>
2023-06-10 10:45:28 +0700

    menyelesaikan news page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import HomePage from "./pages/homepage/homePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewsPage from "./pages/newspage/newsPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/news" element={<NewsPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/newspage/newsPage.js
@@ -0,0 +1,57 @@
+import Terpopuler from "../../components/molecules/terpopuler/terpopuler";
+import Footer from "../../components/organisms/footer/footer";
+import Header from "../../components/organisms/header/header";
+import image from "../../assets/budhis.png";
+
+export default function NewsPage() {
+  return (
+    <>
+      <Header />
+      <div className="news-component w-full sm:flex sm:max-w-6xl mx-auto gap-4 mt-10">
+        <div className="news-component-first basis-8/12 px-2">
+          <div class="categori bg-red-news w-fit p-4 rounded-r-xl ">
+            <p className=" text-white-color text-lg font-bold">Nasional</p>
+          </div>
+          <div className="mt-8">
+            <img src={image} alt="budhis" />
+            <div class="main-news text-lg flex flex-col gap-4 my-4">
+              <p>
+                <strong>LITERASI BANGSA –</strong> Pernyataan Presiden Joko Widodo (Jokowi) soal ikut cawe-cawe politik menjelang Pemilihan Umum 2023 (Pemilu 2024) menuai kontroversi. Jokowi akhirnya memberikan klarifikasi soal
+                pernyataannya saat menerima jajaran Korps Alumni Himpunan Mahasiswa Islam (KAHMI) di Istana Kepresidenan, Jakarta, pada Senin, 5 Juni 2023.
+              </p>
+              <p>Klarifikasi Jokowi tersebut disampaikan Koordinator Presidium Majelis Nasional KAHMI Ahmad Doli Kurnia usai bertemu di Istana Kepresidenan.</p>
+              <p>
+                “Tadi beliau sampaikan klarifikasi soal cawe-cawe, jadi konteksnya Presiden sebetulnya ingin menyampaikan kepada siapa pun yang akan menjadi penerus atau presiden dan wakil presiden pada periode akan datang, itu harus
+                mempunyai persepsi yang sama dalam menghadapi realitas yang tidak mudah, tetapi peluangnya besar,” kata Ahmad Doli.
+              </p>
+              <p>
+                Dijelaskan lebih lanjut, Indonesia tengah menghadapi momentum untuk mendapatkan lompatan luar biasa karena memiliki sumber daya yang dapat membawa Indonesia menjadi negara maju sehingga, menurut Jokowi, semua pihak termasuk
+                Presiden harus terlibat.
+              </p>
+              <p>“Saya kira keterlibatannya bagaimana menyampaikan pesan-pesan untuk kepentingan bangsa dan negara, serta kemajuan yang akan dicapai,” katanya melanjutkan.</p>
+              <p>
+                “Beliau sadar betul tentang posisi beliau sebagai Presiden, Kepala Negara, dan Kepala Pemerintahan. Beliau sadar juga bahwa proses politik pada pemilu lebih melibatkan pimpinan-pimpinan partai politik yang Pak Presiden juga
+                tidak ada di dalamnya,” ujarnya.
+              </p>
+              <p>
+                Menurut Doli, dalam konteks cawe-cawe, keterlibatan Jokowi adalah untuk memastikan semua pihak memahami situasi dan kondisi serta mempunyai tekad untuk maju terus menjadi negara yang lebih baik. Lebih jauh, Jokowi
+                menyampaikan siapapun yang menjadi presiden dan wakil presiden terpilih harus bisa melanjutkan upaya Indonesia melompat menjadi negara yang lebih maju lagi.
+              </p>
+              <p>“Itu ada dalam kurun waktu 10–13 tahun yang akan datang. Proses transisi ini tentu harus dijaga bersama menjadi hal positif, produktif untuk menjadi kemajuan bangsa dan Indonesia ke depan,” ujarnya.</p>
+              <p>Sebelumnya, Jokowi mengucapkan kata cawe-cawe dalam pertemuan sejumlah pimpinan media di Istana Kepresidenan, Jakarta, Senin, 29 Mei 2023.</p>
+              <p>
+                “Harus cawe-cawe untuk tingkat nasional, dia menggarisbawahi bahwa ini tidak ada kaitannya dengan abuse of power sebagai Presiden untuk menjaga bonus demografi dan tidak langsung mengatakan ini siapa,” kata Wakil Pimpinan
+                Redaksi Kompas TV Yogi Nugraha usai bertemu dengan Presiden Jokowi.
+              </p>
+              <p>Menurut Yogi Nugraha, konteks cawe-cawe yang diungkapkan Presiden Jokowi yakni untuk menjaga momentum 13 tahun ke depan saat Indonesia mulai memiliki bonus demografi yang melimpah.</p>
+              <p>“Kemudian dikaitkanlah dengan soal capres, jadi tadi mengatakan begini ‘pemimpin di tahun 2024, 2029, dan 2034 itu sangat krusial untuk mewujudkan 13 tahun,” kata Yogi menirukan Presiden Jokowi.***</p>
+              <p>Editor - ali masykur </p>
+            </div>
+          </div>
+        </div>
+        <Terpopuler className="basis-4/12" />
+      </div>
+      <Footer />
+    </>
+  );
+}
