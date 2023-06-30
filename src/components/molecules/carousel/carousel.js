3f29df329cf0082fedbc0eb4884f1e7f3e19a627
AliMasykur10 <alimasykur2001@gmail.com>
2023-06-30 22:48:05 +0700

    menambahkan title pada carousel

--- src/components/molecules/carousel/carousel.js
+++ src/components/molecules/carousel/carousel.js
@@ -6,7 +6,6 @@ export default function CarouselComp(data) {
   const localhost = "http://localhost:1337";
 
   const dataSliced = data.data.slice(0, 5);
-  // console.log(dataSliced);
   // console.log(localhost + item.attributes.thumbnile.data[0].attributes.url);
   return (
     <>
@@ -15,11 +14,11 @@ export default function CarouselComp(data) {
           {dataSliced.map((item) => {
             return (
               <div className="relative h-full w-full" key={item.id}>
-                <img src={localhost + item.attributes.thumbnile.data[0].attributes.url} alt="1" className="w-full h-96 object-cover" />
+                <img src={localhost + item.attributes.thumbnile.data[0].attributes.url} alt="1" className="w-full h-96 sm:h-[40rem] object-cover" />
                 <div className="absolute inset-0 grid h-full w-full bottom-0 left-0 bg-gradient-to-t from-black">
-                  <div className="w-3/4 md:w-2/4 absolute bottom-5 left-7 sm:left-10">
-                    <Typography variant="h1" color="white" className="sm:mb-4 text-xs  sm:text-4xl ">
-                      Ritual Thudong Biksu Thailand Beri Dampak Ekonomi untuk Candi Borobudur
+                  <div className="w-3/4 md:w-2/4 absolute bottom-10 sm:bottom-2 left-7 sm:left-10">
+                    <Typography variant="h1" color="white" className=" mb-2 sm:mb-4 text-sm  sm:text-2xl ">
+                      {item.attributes.title}
                     </Typography>
                     <Typography variant="lead" color="white" className=" mb-2 sm:mb-12 opacity-80 text-xs sm:text-base">
                       5 Juni 2023
