9bb2504f6df64c6959cf78caadd73748986fec66
AliMasykur10 <alimasykur2001@gmail.com>
2023-06-30 22:34:01 +0700

    menyelesaikan carousel

--- src/components/molecules/carousel/carousel.js
+++ src/components/molecules/carousel/carousel.js
@@ -1,52 +1,34 @@
+import { useEffect, useState } from "react";
 import budhis from "../../../assets/budhis.png";
 import { Carousel, Typography } from "@material-tailwind/react";
 
 export default function CarouselComp(data) {
-  console.log(data.data);
+  const localhost = "http://localhost:1337";
+
+  const dataSliced = data.data.slice(0, 5);
+  // console.log(dataSliced);
+  // console.log(localhost + item.attributes.thumbnile.data[0].attributes.url);
   return (
     <>
       <div className="carousel p-2 sm:p-0">
         <Carousel className="">
-          <div className="relative h-full w-full">
-            <img src={budhis} alt="1" className="w-full object-cover" />
-            <div className="absolute inset-0 grid h-full w-full bottom-0 left-0 bg-gradient-to-t from-black">
-              <div className="w-3/4 md:w-2/4 absolute bottom-5 left-7 sm:left-10">
-                <Typography variant="h1" color="white" className="sm:mb-4 text-xs  sm:text-4xl ">
-                  Ritual Thudong Biksu Thailand Beri Dampak Ekonomi untuk Candi Borobudur
-                </Typography>
-                <Typography variant="lead" color="white" className=" mb-2 sm:mb-12 opacity-80 text-xs sm:text-base">
-                  5 Juni 2023
-                </Typography>
+          {dataSliced.map((item) => {
+            return (
+              <div className="relative h-full w-full" key={item.id}>
+                <img src={localhost + item.attributes.thumbnile.data[0].attributes.url} alt="1" className="w-full h-96 object-cover" />
+                <div className="absolute inset-0 grid h-full w-full bottom-0 left-0 bg-gradient-to-t from-black">
+                  <div className="w-3/4 md:w-2/4 absolute bottom-5 left-7 sm:left-10">
+                    <Typography variant="h1" color="white" className="sm:mb-4 text-xs  sm:text-4xl ">
+                      Ritual Thudong Biksu Thailand Beri Dampak Ekonomi untuk Candi Borobudur
+                    </Typography>
+                    <Typography variant="lead" color="white" className=" mb-2 sm:mb-12 opacity-80 text-xs sm:text-base">
+                      5 Juni 2023
+                    </Typography>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-
-          <div className="relative h-full w-full">
-            <img src={budhis} alt="1" className="w-full object-cover" />
-            <div className="absolute inset-0 grid h-full w-full bottom-0 left-0 bg-gradient-to-t from-black">
-              <div className="w-3/4 md:w-2/4 absolute bottom-5 left-7 sm:left-10">
-                <Typography variant="h1" color="white" className="sm:mb-4 text-xs  sm:text-4xl ">
-                  Ritual Thudong Biksu Thailand Beri Dampak Ekonomi untuk Candi Borobudur
-                </Typography>
-                <Typography variant="lead" color="white" className=" mb-2 sm:mb-12 opacity-80 text-xs sm:text-base">
-                  5 Juni 2023
-                </Typography>
-              </div>
-            </div>
-          </div>
-          <div className="relative h-full w-full">
-            <img src={budhis} alt="1" className="w-full object-cover" />
-            <div className="absolute inset-0 grid h-full w-full bottom-0 left-0 bg-gradient-to-t from-black">
-              <div className="w-3/4 md:w-2/4 absolute bottom-5 left-7 sm:left-10">
-                <Typography variant="h1" color="white" className="sm:mb-4 text-xs  sm:text-4xl ">
-                  Ritual Thudong Biksu Thailand Beri Dampak Ekonomi untuk Candi Borobudur
-                </Typography>
-                <Typography variant="lead" color="white" className=" mb-2 sm:mb-12 opacity-80 text-xs sm:text-base">
-                  5 Juni 2023
-                </Typography>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </Carousel>
       </div>
     </>
--- src/components/organisms/main/main.js
+++ src/components/organisms/main/main.js
@@ -7,7 +7,7 @@ export default function Main(data) {
   return (
     <>
       <main className="w-full sm:max-w-6xl mx-auto mt-10">
-        <CarouselComp data={data.data} />;
+        <CarouselComp data={data.data} />
         <div className="content md:flex md:flex-row md:gap-10 mt-10">
           <NewsSummary titleNews={"Berita Terkini"} />
           <Terpopuler />
